b3116a83d3827025f584960aca254188b49ff566
bilkis38 <bilkisnisa38@gmail.com>
2023-07-14 15:28:10 +0700

    halaman home

---
+++ src/views/FoodsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="FoodsView">
+    <NavbarWa />
+    <h1>Daftar Makanan</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarWa from '@/components/NavbarWa.vue'
+
+export default {
+  name: 'FoodsView',
+  components: {
+    NavbarWa
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <NavbarWa />
+    <div class="container">
+      <HeroWa />
+    </div>    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarWa from '@/components/NavbarWa.vue'
+import HeroWa from '@/components/HeroWa.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavbarWa,
+    HeroWa
+  }
+}
+</script>

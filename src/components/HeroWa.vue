781307525d5f7794e2d92cc95ceda50ad9154d0f
bilkis38 <bilkisnisa38@gmail.com>
2023-07-17 15:30:44 +0700

    first commit

--- src/components/HeroWa.vue
+++ src/components/HeroWa.vue
@@ -1,26 +1,56 @@
 <template>
-  <div class="row mt-4"> 
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2><strong>Delicious Food Menu,</strong> <br> in Your Gadget</h2>
-          <p>Ayo segera pilih da pesan makanan favorit Anda</p>
-          <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>Pesan</button>
-      </div>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo segera pilih da pesan makanan favorit Anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right>Pesan
+              </button>
+            </div>
+          </div>
         </div>
-         </div>
-    <div class="col">
-          <img src="../assets/images/hero.png" width="100%"/>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+      </div>
     </div>
-  </div>
+
+    <!-- Mobile -->
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo segera pilih da pesan makanan favorit Anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right>Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HeroWa'
-}
+  name: "HeroWa",
+};
 </script>
 
 <style>
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,20 +3,60 @@
     <NavbarWa />
     <div class="container">
       <HeroWa />
-    </div>    
+
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Best <strong> FoodsView </strong></h2>
+        </div>
+        <router-link to="/FoodsView" class="btn btn-successs float-right"
+          ><b-icon-eye></b-icon-eye>Lihat Semua
+        </router-link>
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-md-3 mt-4">
+        <CardProductWa />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import NavbarWa from '@/components/NavbarWa.vue'
-import HeroWa from '@/components/HeroWa.vue'
+import NavbarWa from "@/components/NavbarWa.vue";
+import HeroWa from "@/components/HeroWa.vue";
+import CardProductWa from "@/components/CardProductWa.vue";
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     NavbarWa,
-    HeroWa
-  }
-}
+    HeroWa,
+    CardProductWa,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProduct(data) {
+      this.product = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("https://localhost:3000/best-products")
+      .then(function (response) {
+        // handle success
+        console.log("Berhasil : ", response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("Gagal :", error );
+      })
+  },
+};
 </script>
